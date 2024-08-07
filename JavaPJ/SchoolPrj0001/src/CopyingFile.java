349118294c7c8a76e47f22c55e1603a3f171868d
phy0hk <phyoheinko.phk1@gmail.com>
2024-08-07 14:56:56 +0630

    modified:   SchoolPrj0001/src/CopyingFile.java

--- JavaPJ/SchoolPrj0001/src/CopyingFile.java
+++ JavaPJ/SchoolPrj0001/src/CopyingFile.java
@@ -1,7 +1,6 @@
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
 
 public class CopyingFile {
     public static void main(String[] args) {
